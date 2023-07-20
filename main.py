3feb131eee8261c2e9f1be5b3f17cf51f39f30bf
pharaouk <36641995+pharaouk@users.noreply.github.com>
2023-07-20 08:26:00 -0400

    up

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-#main.py
+from finetuner import finetuner_qlora
+from config import ScriptArguments
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Finetune or Load QLora Adapters.')
+    parser.add_argument('--finetune', action='store_true', help='Finetune QLora from datasets')
+    args = parser.parse_args()
+
+    if args.finetune:
+        finetuner_qlora(ScriptArguments)
+
+
+
+if __name__ == "__main__":
+    main()
